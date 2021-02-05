ad220a900776b2092b6510787ac1559a53f50fff
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-05 14:45:11 -0500

    simplified ps script path variable

--- profile.ps1
+++ profile.ps1
@@ -1,17 +1,17 @@
 # Has to be in ...Documents/WindowsPowerShell
 
 # Variables
-$ps_script_dir = 'C:\ps_scripts'
+$psdir = 'C:\ps_scripts'
 
 # General QOL Aliases
 New-Alias touch New-Item
-New-Alias openprof $ps_script_dir/openprofile.ps1
-New-Alias openscripts $ps_script_dir/openps_scripts.ps1
+New-Alias openprof $psdir/openprofile.ps1
+New-Alias openscripts $psdir/openps_scripts.ps1
 
 # Directory Shortcuts
-New-Alias c $ps_script_dir/cd-to-c.ps1
-New-Alias desktop $ps_script_dir/desktop.ps1
-New-Alias localsites $ps_script_dir/localsites.ps1
+New-Alias c $psdir/cd-to-c.ps1
+New-Alias desktop $psdir/desktop.ps1
+New-Alias localsites $psdir/localsites.ps1
 
 # Program Shortcuts
 New-Alias chrome 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
@@ -21,8 +21,8 @@ New-Alias local 'C:\Program Files (x86)\Local\Local.exe'
 # Needs to be updated to current project/more modular
 # RLM Workflow Setup
     # RLM Setup Scripts
-    New-Alias all $ps_script_dir/webdev-startup.ps1 
-    New-Alias rlm $ps_script_dir/rlm-code.ps1
+    New-Alias all $psdir/webdev-startup.ps1 
+    New-Alias rlm $psdir/rlm-code.ps1
 
     
 
