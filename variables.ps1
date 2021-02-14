13558c66199064dc86d7515ab51984fa31ad2327
Dillon Ellis <dillonzellis@gmail.com>
2021-02-13 23:53:01 -0500

    laptop top paths and variables

--- variables.ps1
+++ variables.ps1
@@ -1,8 +1,8 @@
 
 $profile_location = "$users_path/Documents/WindowsPowerShell"
-$users_path = 'C:\Users\d'
+$users_path = 'C:\Users\dillo'
 
 $desktop_path = "$users_path\Desktop\"
 
 $psdir = 'C:\ps_scripts'
-$users_path = 'C:\Users\d\'
+$users_path = 'C:\Users\dillo\'
